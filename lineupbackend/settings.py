a2a111204bb5e7c1379da704ff755e4a24c767fe
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2024-09-01 13:59:43 +0530

    origin

--- lineupbackend/settings.py
+++ lineupbackend/settings.py
@@ -160,6 +160,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS=[
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://lineup-nu.vercel.app/login',
+    'https://lineup-nu.vercel.app',
 ]
 CORS_ALLOWS_CREDENTIALS = True
