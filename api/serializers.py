7267844afe9d1d4f328f19a3bc59f3c54d2ad832
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2024-10-29 09:38:10 +0530

    styles

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
+import re
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,8 +13,23 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
+    def validate_password(self, value):
+        # Check for length
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        
+        # Check for at least one letter, one number, and one special character
+        if (not re.search(r'[A-Za-z]', value) or
+            not re.search(r'[0-9]', value) or
+            not re.search(r'[@$!%*#?&]', value)):
+            raise serializers.ValidationError(
+                "Password must contain at least one letter, one number, and one special character."
+            )
+
+        return value
+    
     def create(self, validated_data):
-        print(validated_data)
+        # print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
 
